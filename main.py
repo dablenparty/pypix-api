f01a81b5f1ae0233910c334ee0757272b67f5917
dablenparty <hdavenport8902@gmail.com>
2024-07-19 13:33:47 -0400

    Add CORSMiddleware, TUS api route

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 ﻿from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from tusserver.tus import create_api_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(
+    create_api_router(
+        files_dir="./images",
+    )
+)
 
 
 @app.get("/")
