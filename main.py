2734bf17f3cbe94db5ae66e1508f80a4950ec15a
dablenparty <hdavenport8902@gmail.com>
2024-07-20 21:05:01 -0400

    Forked tuspy-server, switched to that

--- main.py
+++ main.py
@@ -41,9 +41,8 @@ app.add_middleware(
 app.include_router(
     create_api_router(
         files_dir="./images",
-        location="http://127.0.0.1:8000/images/upload",
+        prefix="/images/upload",
     ),
-    prefix="/images/upload",
 )
 
 app.include_router(images_router)
