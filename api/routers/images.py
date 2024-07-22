ab7740c3f4d80f40fe1f1f4227cdaa626b37df61
dablenparty <hdavenport8902@gmail.com>
2024-07-22 18:47:04 -0400

    Refactor get_image to use HTTPException

--- api/routers/images.py
+++ api/routers/images.py
@@ -1,21 +1,27 @@
 ﻿import uuid
 
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, Response, HTTPException
+from starlette import status
 
 from tus_utils import get_image_path, get_image_metadata
 
 images_router = APIRouter(
     prefix="/api/v1/images",
     tags=["images"],
-    responses={404: {"description": "Not found"}},
+    responses={status.HTTP_404_NOT_FOUND: {"message": "Not found"}},
 )
 
 
-@images_router.get("/{image_id}", responses={200: {"image": {}}}, response_class=Response)
+@images_router.get("/{image_id}", response_class=Response, status_code=status.HTTP_200_OK)
 def get_image(image_id: uuid.UUID):
-    metadata = get_image_metadata(image_id)
-    media_type = metadata.metadata.get("filetype", "image/jpeg")
+    try:
+        metadata = get_image_metadata(image_id)
+        media_type = metadata.metadata.get("filetype", "image/jpeg")
+    except FileNotFoundError as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
     image_path = get_image_path(image_id)
     with image_path.open("rb") as f:
         image_bytes = f.read()
     return Response(content=image_bytes, media_type=media_type)
+
+# TODO: search by embeddings
